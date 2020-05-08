71754a33c0f65af022f2e5974dff5c0a668b8325
jtother <juanramonquiroz15@gmail.com>
2020-05-08 15:16:29 -0500

    created index route show route for hbs

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,65 @@
 const Post = require('../models/posts');
 const express = require('express');
-const Router = express.Router();
+const router = express.Router();
+
+//index route
+router.get('/', (req, res) => {
+	Post.find({})
+		.then((posts) => {
+			res.render('index', {
+				posts
+			});
+		})
+		.catch(console.error);
+});
+
+//new route
+router.get('/new', (req, res) => {
+	res.render('new');
+})
+
+//edit route
+router.get('/:id/edit', (req, res) => {
+	const id = req.params.id;
+	Post.findById(id)
+		.then(post => {
+			res.render('edit', post);
+		})
+		.catch(console.error);
+});
+//put route
+router.put('/:id', (req, res) => {
+	Post.findOneAndUpdate({
+			_id: req.params.id
+		}, req.body, {
+			new: true
+		})
+		.then((post) => {
+			res.redirect('/posts');
+		})
+		.catch(console.error);
+});
+
+
+
+//show
+router.get('/:id', (req, res) => {
+	Post.findById(req.params.id)
+		.then((post) => {
+			res.render('show', post);
+		})
+		.catch(console.error);
+});
+
+router.post('/', (req, res) => {
+	Post.create(req.body)
+	  .then(post => {
+		res.redirect('/posts');
+	  })
+	  .catch(console.error);
+  });
+
+
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+const postsController = require('./controllers/posts');
 const express = require('express');
+const methodOverride = require('method-override');
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.set('view engine', 'hbs');
-
+app.use(methodOverride('_method'));
+app.use('/posts', postsController);
 app.listen(4000, () => {
-	console.log('app listening on port');
+	console.log('app listening on port 4000');
 });
